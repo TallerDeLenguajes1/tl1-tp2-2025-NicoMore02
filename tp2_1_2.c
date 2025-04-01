999e54248a18e20f0bf6cb33039fc84ac582ae42
NicoMore02 <nicomore053@gmail.com>
2025-04-01 13:01:56 -0300

    Genera numeros aleatorios y correciones en tp2_1_1

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -7,12 +7,16 @@
 int main() {
     int i;
     double vt[N];
+    double *punterovt = vt;
+
     srand(time(NULL));
+
     for ( i = 0; i < N; i++)
     {
-        vt[i] = 1 + rand() % 100;
-        printf("%f\n  ", vt[i]);
+        *(punterovt + i) = 1 + rand() % 100;
+        printf("%f\n  ", punterovt[i]);
     }
+    
     
     return 0;
 }
